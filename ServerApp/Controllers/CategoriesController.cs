f5eeba5fdbc13fb2b7606eb33b9cc69025bc2468
unknown <hradoevsoft@gmail.com>
2023-10-16 14:33:20 +0300

    Added ServerApp

---
+++ ServerApp/Controllers/CategoriesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HrEmbros.Models;
+//using HrEmbros.DependencyInjection;
+
+namespace HrEmbros.Controllers
+{
+
+
+    [Route("api/categories")]
+    [ApiController]
+
+    public class CategoriesController : ControllerBase
+    {
+        //public string[] categories = { "Начало", "Текст", "Фолк/кръстат бод", "Фото" };
+        //private IShoppingCartService _shoppingCartService;
+        public List<Category> categories = new List<Category>
+            { new Category {CId = "Lettering", Name = "Текст"},
+              new Category {CId = "Cross", Name = "Фолк/Кръстат бод" },
+              new Category {CId = "Photo", Name = "Фото бод" },
+              new Category {CId = "Adobe", Name = "Adobe Ai" },
+              new Category {CId = "Product_list", Name = "Списък с продукти" },
+              new Category {CId = "Histogram_plot", Name = "Статистика" }};
+
+
+//        public CategoriesController(IShoppingCartService shoppingCartService)
+        public CategoriesController()
+        {
+            //_shoppingCartService = shoppingCartService;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            //return categories;
+            //return Json(categories);
+            //return new JsonResult(new { categories = categories, user = (_shoppingCartService as ShoppingCartService).User});
+            return new JsonResult(new { categories = categories, user = "" });
+            //return new JsonResult(new { foo = "bar", baz = "Blech", emb_id = 10 });
+        }
+
+    }
+}
---
+++ ServerApp/Models/Product.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace HrEmbros.Models
+{
+ /*   public class Product
+    {
+        public long PId { get;  set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }*/
+
+    public class Product
+    {
+        [Key]
+        public int PId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Size { get; set; }
+        public string ColorCount { get; set; }
+        public string StitchCount { get; set; }
+        public string Category { get; set; }
+        public string UrlSmallImage { get; set; }
+        public string UrlBigImage { get; set; }
+        public string AltImage { get; set; }
+        public string EmbFile { get; set; }
+        public bool EnableDownload { get; set; }
+        public int Prior { get; set; }
+    }
+
+    public class Category
+    {
+        public string CId { get; set; } //=>[Lettering, Cross]
+        public string Name { get; set; } //=>[Текст, Фолк/Кръстат бод]
+    }
+
+    public class DataTableProduct
+    {
+        [Key]
+        public int PId { get; set; }
+        public string Name { get; set; }
+        public string Size { get; set; }
+    }
+}
